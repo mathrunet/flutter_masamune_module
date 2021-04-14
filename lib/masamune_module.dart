fb07bb5636494da2cc2a0b9b9950417e09860aa8
mathru <user@mathru.net>
2021-04-14 18:57:55 +0900

    モジュール作成中

--- lib/masamune_module.dart
+++ lib/masamune_module.dart
@@ -11,6 +11,6 @@ library masamune_module;
 import 'dart:async';
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:masamune/masamune.dart';
 import 'package:share/share.dart';
-
